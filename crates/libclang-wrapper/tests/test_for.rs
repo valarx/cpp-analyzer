34c9d20f05c94d0646216fc1df5a4252e53459d9
valar <valar1989@gmail.com>
2020-04-27 12:44:53 +0200

    feat: Add tests for loop without initializer

--- crates/libclang-wrapper/tests/test_for.rs
+++ crates/libclang-wrapper/tests/test_for.rs
@@ -5,7 +5,7 @@ use libclang_wrapper::source::{
 };
 
 #[test]
-fn test_parsing_if_conditions() {
+fn test_classic_for_loop() {
     let source = Source::from_file(
         "tests/for.cpp".to_owned(),
         DeclarationFromPHCMode::Exclude,
@@ -244,6 +244,244 @@ fn test_parsing_if_conditions() {
                             }
                         ]
                     }]
+                }]
+            }]
+        }
+    );
+}
+
+#[test]
+fn test_for_no_initializer() {
+    let source = Source::from_file(
+        "tests/for_no_init.cpp".to_owned(),
+        DeclarationFromPHCMode::Exclude,
+        DiagnosticsMode::Enabled,
+        vec!["-x".to_owned(), "c++".to_owned()],
+        TUOptionsBuilder::new(),
+    )
+    .unwrap();
+    let translation_units: Result<Vec<_>, _> = source.translation_units.into_iter().collect();
+    let translation_unit = translation_units.unwrap().into_iter().nth(0).unwrap();
+    let ast = translation_unit.ast();
+    assert_eq!(
+        ast,
+        &Entry {
+            current_kind: CursorKind::Root,
+            children: vec![Entry {
+                current_kind: CursorKind::Function {
+                    spelling: "f".to_owned(),
+                    code_span: CodeSpan {
+                        start_pos: Position {
+                            file_name: "tests/for_no_init.cpp".to_owned(),
+                            line: 1,
+                            col: 1
+                        },
+                        end_pos: Position {
+                            file_name: "tests/for_no_init.cpp".to_owned(),
+                            line: 5,
+                            col: 2
+                        }
+                    },
+                    cur_type: CursorType::FunctionProto,
+                    return_type: CursorType::Void,
+                    canonical_return_type: CursorType::Void
+                },
+                children: vec![Entry {
+                    current_kind: CursorKind::CompoundStatement(CodeSpan {
+                        start_pos: Position {
+                            file_name: "tests/for_no_init.cpp".to_owned(),
+                            line: 1,
+                            col: 10
+                        },
+                        end_pos: Position {
+                            file_name: "tests/for_no_init.cpp".to_owned(),
+                            line: 5,
+                            col: 2
+                        }
+                    }),
+                    children: vec![
+                        Entry {
+                            current_kind: CursorKind::DeclarationStatement(CodeSpan {
+                                start_pos: Position {
+                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                    line: 2,
+                                    col: 3
+                                },
+                                end_pos: Position {
+                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                    line: 2,
+                                    col: 13
+                                }
+                            }),
+                            children: vec![Entry {
+                                current_kind: CursorKind::Variable(
+                                    "i".to_owned(),
+                                    CodeSpan {
+                                        start_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 2,
+                                            col: 3
+                                        },
+                                        end_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 2,
+                                            col: 12
+                                        }
+                                    },
+                                    CursorType::Int
+                                ),
+                                children: vec![Entry {
+                                    current_kind: CursorKind::IntegerLiteral(CodeSpan {
+                                        start_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 2,
+                                            col: 11
+                                        },
+                                        end_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 2,
+                                            col: 12
+                                        }
+                                    }),
+                                    children: vec![]
+                                }]
+                            }]
+                        },
+                        Entry {
+                            current_kind: CursorKind::ForStatement(CodeSpan {
+                                start_pos: Position {
+                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                    line: 3,
+                                    col: 3
+                                },
+                                end_pos: Position {
+                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                    line: 4,
+                                    col: 4
+                                }
+                            }),
+                            children: vec![
+                                Entry {
+                                    current_kind: CursorKind::BinaryOperator(CodeSpan {
+                                        start_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 3,
+                                            col: 10
+                                        },
+                                        end_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 3,
+                                            col: 15
+                                        }
+                                    }),
+                                    children: vec![
+                                        Entry {
+                                            current_kind: CursorKind::UnexposedExpression(
+                                                "i".to_owned(),
+                                                CodeSpan {
+                                                    start_pos: Position {
+                                                        file_name: "tests/for_no_init.cpp"
+                                                            .to_owned(),
+                                                        line: 3,
+                                                        col: 10
+                                                    },
+                                                    end_pos: Position {
+                                                        file_name: "tests/for_no_init.cpp"
+                                                            .to_owned(),
+                                                        line: 3,
+                                                        col: 11
+                                                    }
+                                                }
+                                            ),
+                                            children: vec![Entry {
+                                                current_kind:
+                                                    CursorKind::DeclarationReferenceExpression(
+                                                        "i".to_owned(),
+                                                        CodeSpan {
+                                                            start_pos: Position {
+                                                                file_name: "tests/for_no_init.cpp"
+                                                                    .to_owned(),
+                                                                line: 3,
+                                                                col: 10
+                                                            },
+                                                            end_pos: Position {
+                                                                file_name: "tests/for_no_init.cpp"
+                                                                    .to_owned(),
+                                                                line: 3,
+                                                                col: 11
+                                                            }
+                                                        }
+                                                    ),
+                                                children: vec![]
+                                            }]
+                                        },
+                                        Entry {
+                                            current_kind: CursorKind::IntegerLiteral(CodeSpan {
+                                                start_pos: Position {
+                                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                                    line: 3,
+                                                    col: 14
+                                                },
+                                                end_pos: Position {
+                                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                                    line: 3,
+                                                    col: 15
+                                                }
+                                            }),
+                                            children: vec![]
+                                        }
+                                    ]
+                                },
+                                Entry {
+                                    current_kind: CursorKind::UnaryOperator(CodeSpan {
+                                        start_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 3,
+                                            col: 17
+                                        },
+                                        end_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 3,
+                                            col: 20
+                                        }
+                                    }),
+                                    children: vec![Entry {
+                                        current_kind: CursorKind::DeclarationReferenceExpression(
+                                            "i".to_owned(),
+                                            CodeSpan {
+                                                start_pos: Position {
+                                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                                    line: 3,
+                                                    col: 19
+                                                },
+                                                end_pos: Position {
+                                                    file_name: "tests/for_no_init.cpp".to_owned(),
+                                                    line: 3,
+                                                    col: 20
+                                                }
+                                            }
+                                        ),
+                                        children: vec![]
+                                    }]
+                                },
+                                Entry {
+                                    current_kind: CursorKind::CompoundStatement(CodeSpan {
+                                        start_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 3,
+                                            col: 22
+                                        },
+                                        end_pos: Position {
+                                            file_name: "tests/for_no_init.cpp".to_owned(),
+                                            line: 4,
+                                            col: 4
+                                        }
+                                    }),
+                                    children: vec![]
+                                }
+                            ]
+                        }
+                    ]
                 }]
             }]
         }
